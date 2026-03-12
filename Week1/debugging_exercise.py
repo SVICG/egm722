85e485ac9a7c791a5390543d315e2bdbc2777745
Steph <thecrypttrip@gmail.com>
2026-03-12 19:52:00 +0000

    Created variable
    Variable to store number of guesses

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -5,6 +5,7 @@ rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
+guessCount = 0 #variable to store guesses
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess < rand:  # if the guess is too high, tell the user.
