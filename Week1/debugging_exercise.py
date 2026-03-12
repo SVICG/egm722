a7204e5a803f8da2651f76f0a01b2f38f9b220ed
Steph <thecrypttrip@gmail.com>
2026-03-12 20:02:21 +0000

    Added counting of guesses
    - increment number of guesses with each loop
    updated print statement to include number of guesses

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -5,9 +5,11 @@ rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
-guessCount = 0 #variable to store guesses
+guessCount = 1 #variable to store guesses
 
-while guess != rand:  # if the guess is not equal to the random number, you have to guess again
+while guess != rand:
+    guessCount += 1 #increment count with each loop
+    # if the guess is not equal to the random number, you have to guess again
     if guess < rand:  # if the guess is too high, tell the user.
         print('Too low. Guess again.')
     else:  # if the guess is too low, tell the user.
@@ -16,4 +18,4 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     print('Enter a new guess: ')
     guess = int(input())
 
-print('You got it! The number was {}'.format(rand))
+print('You got it in {}'.format(guessCount)+'! The number was {}'.format(rand))
